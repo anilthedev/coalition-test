6c2b93948532974453c9e2b9cd23d6b3242d2188
anilthedev <30975583+anilthedev@users.noreply.github.com>
2017-08-13 18:34:29 +0530

    Added product blade

---
+++ resources/views/product.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Sample Bootstrap Page</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+<div class="container">
+    <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills float-right">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+               {{-- <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>--}}
+            </ul>
+        </nav>
+        <h3 class="text-muted">Project name</h3>
+    </div>
+
+    <div class="jumbotron">
+        <h1 class="display-3">Add Your Product</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    1 of 2
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    <footer class="footer">
+        <p>&copy; Company 2017</p>
+    </footer>
+
+</div> <!-- /container -->
+
+</body>
+</html>
